d6eec1ce199de3113931beab5322c76159169093
unknown <paulomoabe13@gmail.com>
2024-03-12 16:58:33 -0300

    alteracao em cotrollers, routes, em duas views

--- controllers/TaskController.mjs
+++ controllers/TaskController.mjs
@@ -1,28 +1,27 @@
-// Importa o modelo Task do arquivo Task.mjs, que representa a estrutura e o comportamento de uma tarefa no aplicativo
 import { raw } from 'express';
 import Task from '../models/Task.mjs';
 
-// Define a classe TaskController, que será responsável por lidar com as solicitações relacionadas às tarefas no aplicativo
+
 export default class TaskController {
    
-    // Método estático createTask, que renderiza uma página para criar uma nova tarefa
+    
     static createTask(req, res) {
-        res.render('tasks/create'); // Renderiza a view 'tasks/create' como resposta à requisição
+        res.render('tasks/create');
     }
     
-  // Método estático assíncrono createTaskSave, que cria e salva uma nova tarefa no banco de dados
+ 
     static async createTaskSave(req, res) {
-    // Cria um objeto 'task' com base nos dados enviados no corpo da requisição (req.body)
+   
         const task = {
-            title: req.body.title,          // Obtém o título da tarefa do corpo da requisição
-            description: req.body.description,  // Obtém a descrição da tarefa do corpo da requisição
-            done: false,                    // Define o estado 'done' como falso (indicando que a tarefa não está concluída)
+            title: req.body.title,          
+            description: req.body.description, 
+            done: false,                    
         };
 
-        // Aguarda a criação da nova tarefa no banco de dados usando o método Task.create()
+       
         await Task.create(task);
 
-        // Redireciona o usuário para a rota '/tasks' após criar a nova tarefa
+       
         res.redirect('/tasks');
     }
 
@@ -49,7 +48,7 @@ export default class TaskController {
         const id = req.body.id
 
         const task = {
-            done: req.body.done === '0' ? true : false
+            done: req.body.done === '0' ? true : false,
         }
 
         await Task.update(task, {where:{id: id}})
@@ -57,11 +56,24 @@ export default class TaskController {
         res.redirect('/tasks')
     }
 
-    // Método estático showTasks, que renderiza uma página mostrando todas as tarefas existentes
+    static async updateTaskPost(req, res){
+
+        const id = req.body.id
+
+        const task = {
+            title: req.body.title,
+            description: req.body.description,
+        }
+
+        await Task.update(task, {where: {id: id}})
+
+        res.redirect('/tasks')
+    }
+    
     static async showTasks(req, res) {
 
         const tasks = await Task.findAll({raw: true })
 
-        res.render('tasks/all', {tasks}); // Renderiza a view 'tasks/all' como resposta à requisição
+        res.render('tasks/all', {tasks}); 
     }
 }
